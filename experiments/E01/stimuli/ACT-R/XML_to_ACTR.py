726ebd7dcfc92c14a09045693d5c121e68fd2dc8
pjrice <pjr@rice.edu>
2022-02-22 16:49:15 -0600

    XMLtoACTR2 function opens an experiment window and adds lines with actr.add_line_to_exp_window to represent xml file mxGeometry elements, instead of adding directly to the visicon (which is what XMLtoACTR function does)

--- experiments/E01/stimuli/ACT-R/XML_to_ACTR.py
+++ experiments/E01/stimuli/ACT-R/XML_to_ACTR.py
@@ -12,12 +12,47 @@ def parseXMLtoACTR(xmlFile):
     # get root element
     root = tree.getroot()
     
+    # for each mxGeometry element in the xml file
     for item in root.iter('mxGeometry'):
         
+        # get x/y and height/width of the mxGeometry element
         xCoord = item.get('x')
         yCoord = item.get('y')
         height = item.get('height')
         width = item.get('width')
         
+        # add a visicon feature that reflects the x/y and height/width of the mxGeometry element
         actr.add_visicon_features(['SCREEN-X',xCoord,'SCREEN-Y',yCoord,'HEIGHT',height,'WIDTH',width])
+        
+        
+        
+def parseXMLtoACTR2(xmlFile):
+    
+    # create element tree object
+    tree = ET.parse(xmlFile)
+  
+    # get root element
+    root = tree.getroot()
+    
+    # open an act window and install the device
+    window = actr.open_exp_window('test', width=850, height=1100, visible=True)
+    actr.install_device(window)
+    
+    # for each mxGeometry element in the xml file
+    for item in root.iter('mxGeometry'):
+        
+        # get x/y and height/width of the mxGeometry element
+        xCoord = int(item.get('x'))
+        yCoord = int(item.get('y'))
+        height = int(item.get('height'))
+        width = int(item.get('width'))
+        
+        # currently, all mxGeometry elements are squares/rectangles
+        # compute the x/y start/end points of the four lines that make up the element and add
+        # them to the experiment window
+        actr.add_line_to_exp_window(window,[xCoord,yCoord],[xCoord+width,yCoord])
+        actr.add_line_to_exp_window(window,[xCoord,yCoord],[xCoord,yCoord+height])
+        actr.add_line_to_exp_window(window,[xCoord+width,yCoord],[xCoord+width,yCoord+height])
+        actr.add_line_to_exp_window(window,[xCoord,yCoord+height],[xCoord+width,yCoord+height])
+        
 
