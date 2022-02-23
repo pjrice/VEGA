138524b52137ccc45f5564074cfb87bf37352560
pjrice <pjr@rice.edu>
2022-02-23 11:57:48 -0600

    figuring out how x/y position of features added to experiment window are converted to x/y position in visicon

---
+++ expWindow-to-visicon-test.lisp
@@ -0,0 +1,15 @@
+(load "/home/ausmanpa/actr7.x/load-act-r.lisp")
+
+(run-environment)
+
+(define-model dummyModel (sgp :force-visual-commands t))
+
+(defvar *window*)
+
+
+(setf *window* (open-exp-window "test" :width 500 :height 500))
+
+(install-device *window*)
+
+
+(add-line-to-exp-window *window* (list 0 0) (list 500 500))
