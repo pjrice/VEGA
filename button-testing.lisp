3654cfcedb90f058e4dde6ed04e9075cbcbb4a6a
pjrice <pjr@rice.edu>
2022-02-24 16:47:17 -0600

    button-testing.lisp now provides a working example of creating multiple toggle-able buttons on an ACT-R experiment window

--- button-testing.lisp
+++ button-testing.lisp
@@ -1,29 +1,33 @@
+(defvar *window* nil)
+(defparameter button-state (make-hash-table))
 
-
+(defun on-button-press (button)
+    (if (= (gethash button button-state) 0)
+        (progn
+            (modify-button-for-exp-window button :color 'black)
+            (setf (gethash button button-state) 1))
+        (progn
+            (modify-button-for-exp-window button :color 'white)
+            (setf (gethash button button-state) 0))
+    )
+)
 
 (defun button-test ()
 
-    (let ((test-window (open-exp-window "test" :visible t :width 500 :height 500)))
+    (setf *window* (open-exp-window "test" :visible t :width 500 :height 500))
     
-        ; try a lambda function - this results in:
-        ;"#|Warning: Error while calling "agi-win-click-mouse-monitor461" as a monitor for "click-mouse": Error #<TOO-FEW-ARGUMENTS Too few arguments in call to #<Compiled-function (:INTERNAL BUTTON-TEST) (Non-Global)  #x302001AD5E3F>: 0 arguments provided, at least 1 required. > occurred while trying to evaluate command "agi-win-click-mouse-monitor461" with parameters (NIL (558 557) INDEX) |#"
-        (setf test-button1 (add-button-to-exp-window test-window :color 'white :text "" :x 200 :y 250 :width 20 :height 10 :action (lambda (button) (modify-button-for-exp-window button :color 'black))))
+    (let* (
+        (xCoord 200))
         
-        ; try modifying the button with an action after it is initialized - this results in:
-        ; "#|Warning: Error while calling "agi-win-click-mouse-monitor473" as a monitor for "click-mouse": Error #<TOO-FEW-ARGUMENTS Too few arguments in call to #<Compiled-function BUTTON-PRESS #x3020018E59DF>: 0 arguments provided, at least 1 required. > occurred while trying to evaluate command "agi-win-click-mouse-monitor473" with parameters (NIL (557 557) INDEX) |#"
-        ; effectively the same as the lambda function
-        (setf test-button2 (add-button-to-exp-window test-window :color 'white :text "" :x 250 :y 250 :width 20 :height 10 :action nil))
-        (modify-button-for-exp-window test-button2 :action (list 'button-press test-button2))
-        ;(modify-button-for-exp-window test-button2 :action (list "test-button-pressed" test-button2)) ; using a command results in the same as using the function
-        
+        (loop 
+            (let* (
+                (b (add-button-to-exp-window *window* :color 'white :text "" :x xCoord :y 250 :width 20 :height 10 :action nil)))
+                
+                (setf (gethash b button-state) 0)
+                (modify-button-for-exp-window b :action (list 'on-button-press b)))
+                
+            (setq xCoord (+ xCoord 50))
+            (when (> xCoord 250) (return xCoord))
+        )
     )
-
 )
-
-(defun button-press (button)
-    (modify-button-for-exp-window button :color 'black)
-)
-
-(add-act-r-command "test-button-pressed" 'button-press)
-
-
