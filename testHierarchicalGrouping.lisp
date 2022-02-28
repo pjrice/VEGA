6b109a356c57264618bf4e10f46cae0703153ade
Patrick Rice <pjr5@rice.edu>
2022-02-28 11:38:40 -0600

    skjdnfg

--- testHierarchicalGrouping.lisp
+++ testHierarchicalGrouping.lisp
@@ -1,11 +1,19 @@
-(setf *default-pathname-defaults* #P"/home/ausmanpa/")
+(setf *default-pathname-defaults* #P"/Users/pjr5/")
 
-(load (merge-pathnames "actr7.x/load-act-r.lisp"))
+(load (merge-pathnames "Desktop/actr7.x/load-act-r.lisp"))
 
 (load (merge-pathnames "Desktop/A-Voting-Folder/combine.lisp"))
 
 (run-environment)
 
+
+
+
 (run-single '("No-Lines-Color" nil nil nil nil nil) t nil t)
 
 (run-single '("No-Lines-Color-Box" nil nil nil nil nil) t nil t)
+
+
+
+
+(vote nil nil t nil)
