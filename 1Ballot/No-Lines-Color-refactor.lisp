c014dcfb98b154faa03b42dec50c8f4ea9ccc0ec
pjrice <pjr@rice.edu>
2022-03-01 16:30:24 -0600

    updating testing script; forgot to add the candidate list to the refactored No-Lines-Color ballot

--- 1Ballot/No-Lines-Color-refactor.lisp
+++ 1Ballot/No-Lines-Color-refactor.lisp
@@ -44,6 +44,245 @@
 )
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Define the list of races: their names, candidates, and parties
+;; Used in the below (vote) function to construct the ballot
+
+(defparameter cntst-lst nil)
+
+; A single race
+(defclass contest ()
+  ((office-name :accessor office-name :initarg :office-name :initform nil)
+
+   (cand-lst :accessor cand-lst :initarg :cand-lst :initform nil)
+   (selection :accessor selection :initarg :selection :initform nil)
+   (office-field :accessor office-field :initarg :office-field)
+	)
+  )
+  
+
+; A single candidate  
+(defclass cand-choice ()
+  ((cand-name :accessor cand-name :initarg :cand-name :initform nil)
+   (party-name :accessor party-name :initarg :party-name :initform "")
+   (selected-p :accessor selected-p :initarg :selected-p :initform nil)
+   (parent :accessor parent :initarg :parent :initform nil)
+   (my-idx :accessor my-idx :initarg :my-idx :initform nil)
+   ))
+
+; The contest list
+(setf cntst-lst
+	  (list
+	   
+	   (make-instance 'contest
+		 :office-name "PresidentoftheUnitedStates"
+		 :cand-lst
+		 (list
+		  (make-instance 'cand-choice
+			:cand-name "GordonBearce" :party-name "REP")
+		  (make-instance 'cand-choice
+			:cand-name "VernonStanleyAlbury" :party-name "DEM")
+		  (make-instance 'cand-choice
+			:cand-name "JanetteFroman" :party-name "LIB")))
+	   
+	   (make-instance 'contest
+		 :office-name "UnitedStatesSenator"
+		 :cand-lst
+		 (list
+		  (make-instance 'cand-choice
+			:cand-name "CecileCadieux" :party-name "REP")
+		  (make-instance 'cand-choice
+			:cand-name "FernBrzezinski" :party-name "DEM")
+		  (make-instance 'cand-choice
+			:cand-name "CoreyDery" :party-name "IND")))
+	   
+	   (make-instance 'contest
+		 :office-name "UnitedStatesRepresentativeDistrict7"
+		 :cand-lst
+		 (list
+		  (make-instance 'cand-choice
+			:cand-name "PedroBrouse" :party-name "REP")
+		  (make-instance 'cand-choice
+			:cand-name "RobertMettler" :party-name "DEM")))
+
+	   (make-instance 'contest
+		 :office-name "Governor"
+		 :cand-lst
+		 (list
+		  (make-instance 'cand-choice
+			:cand-name "GlenTravisLozier" :party-name "REP")
+		  (make-instance 'cand-choice
+			:cand-name "RickStickles" :party-name "DEM")
+		  (make-instance 'cand-choice
+			:cand-name "MauriceHumble" :party-name "IND")))
+
+	   (make-instance 'contest
+		 :office-name "LieutenantGovernor"
+		 :cand-lst
+		 (list
+		  (make-instance 'cand-choice
+			:cand-name "ShaneTerrio" :party-name "REP")
+		  (make-instance 'cand-choice
+			:cand-name "CassiePrincipe" :party-name "DEM")))
+
+	   (make-instance 'contest
+		 :office-name "AttorneyGeneral"
+		 :cand-lst
+		 (list
+		  (make-instance 'cand-choice
+			:cand-name "TimSpeight" :party-name "REP")
+		  (make-instance 'cand-choice
+			:cand-name "RickOrgan" :party-name "DEM")))
+
+	   
+	   (make-instance 'contest
+		 :office-name "ComptrollerofPublicAccounts"
+		 :cand-lst
+		 (list
+		  (make-instance 'cand-choice
+			:cand-name "ThereseGustin" :party-name "IND")
+		  (make-instance 'cand-choice
+			:cand-name "GregConverse" :party-name "DEM")))
+
+	   
+	   (make-instance 'contest
+		 :office-name "CommissionerofGeneralLandOffice"
+		 :cand-lst
+		 (list
+		  (make-instance 'cand-choice
+			:cand-name "SamSaddler" :party-name "REP")
+		  (make-instance 'cand-choice
+			:cand-name "EliseEllzey" :party-name "DEM")))
+
+	   
+	   (make-instance 'contest
+		 :office-name "CommissionerofAgriculture"
+		 :cand-lst
+		 (list
+		  (make-instance 'cand-choice
+			:cand-name "PollyRylander" :party-name "REP")
+		  (make-instance 'cand-choice
+			:cand-name "RobertoAron" :party-name "DEM")))
+
+	   
+	   (make-instance 'contest
+		 :office-name "RailroadCommissioner"
+		 :cand-lst
+		 (list
+		  (make-instance 'cand-choice
+			:cand-name "JillianBalas" :party-name "REP")
+		  (make-instance 'cand-choice
+			:cand-name "ZacharyMinick" :party-name "DEM")))
+
+	   
+	   (make-instance 'contest
+		 :office-name "StateSenator"
+		 :cand-lst
+		 (list
+		  (make-instance 'cand-choice
+			:cand-name "RicardoNigro" :party-name "REP")
+		  (make-instance 'cand-choice
+			:cand-name "WesleyStevenMillette" :party-name "DEM")))
+
+	   
+	   (make-instance 'contest
+		 :office-name "StateRepresentativeDistrict134"
+		 :cand-lst
+		 (list
+		  (make-instance 'cand-choice
+			:cand-name "PetraBencomo" :party-name "REP")
+		  (make-instance 'cand-choice
+			:cand-name "SusanneRael" :party-name "DEM")))
+
+	   
+	   (make-instance 'contest
+		 :office-name "MemberStateBoardofEducationDistrict2"
+		 :cand-lst
+		 (list
+		  (make-instance 'cand-choice
+			:cand-name "PeterVarga" :party-name "REP")
+		  (make-instance 'cand-choice
+			:cand-name "MarkBaber" :party-name "DEM")))
+
+	   
+	   (make-instance 'contest
+		 :office-name "PresidingJudgeTexasSupremeCourtPlace2"
+		 :cand-lst
+		 (list
+		  (make-instance 'cand-choice
+			:cand-name "TimGrasty" :party-name "DEM")))
+
+	   
+	   (make-instance 'contest
+		 :office-name "PresidingJudgeCourtofCriminalAppeals"
+		 :cand-lst
+		 (list
+		  (make-instance 'cand-choice
+			:cand-name "DanPlouffe" :party-name "REP")
+		  (make-instance 'cand-choice
+			:cand-name "DerrickMelgar" :party-name "DEM")))
+
+	   
+	   (make-instance 'contest
+		 :office-name "DistrictAttorney"
+		 :cand-lst
+		 (list
+		  (make-instance 'cand-choice
+			:cand-name "CoreyBehnke" :party-name "REP")
+		  (make-instance 'cand-choice
+			:cand-name "JenniferALundeed" :party-name "DEM")))
+
+	   
+	   (make-instance 'contest
+		 :office-name "CountyTreasurer"
+		 :cand-lst
+		 (list
+		  (make-instance 'cand-choice
+			:cand-name "DeanCaffee" :party-name "REP")
+		  (make-instance 'cand-choice
+			:cand-name "GordonKallas" :party-name "DEM")))
+
+	   
+	   (make-instance 'contest
+		 :office-name "Sheriff"
+		 :cand-lst
+		 (list
+		  (make-instance 'cand-choice
+			:cand-name "StanleySaari" :party-name "REP")
+		  (make-instance 'cand-choice
+			:cand-name "JasonValle" :party-name "DEM")))
+
+	   
+	   (make-instance 'contest
+		 :office-name "CountyTaxAssessor"
+		 :cand-lst
+		 (list
+		  (make-instance 'cand-choice
+			:cand-name "HowardGrady" :party-name "REP")
+		  (make-instance 'cand-choice
+			:cand-name "RandyHClemons" :party-name "DEM")))
+
+	   
+	   (make-instance 'contest
+		 :office-name "JusticeofthePeace"
+		 :cand-lst
+		 (list
+		  (make-instance 'cand-choice
+			:cand-name "DeborahKamps" :party-name "REP")
+		  (make-instance 'cand-choice
+			:cand-name "ClydeGaytonJr" :party-name "DEM")))
+
+	   
+	   (make-instance 'contest
+		 :office-name "CountyJudge"
+		 :cand-lst
+		 (list
+		  (make-instance 'cand-choice
+			:cand-name "DanAtchley" :party-name "REP")
+		  (make-instance 'cand-choice
+			:cand-name "LewisShine" :party-name "DEM")))
+	))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; A ballot function that gets called within combine.lisp. Constructs the ballot on the ACT-R experiment window
 ;; so that a model or human can interact with the ballot.
 ;; realtime is a boolean that specifies whether to run the model in real time or not (only pertanent if use-model is true)
--- quickTest.lisp
+++ quickTest.lisp
@@ -2,16 +2,18 @@
 
 (load (merge-pathnames "actr7.x/load-act-r.lisp"))
 
-(load (merge-pathnames "Desktop/A-Voting-Folder/combine.lisp"))
+(load (merge-pathnames "gp/VEGA/combine.lisp"))
 
 (run-environment)
 
 
 
-;(run-single '("No-Lines-Color" nil nil nil nil nil) nil nil t)
+;(run-single '("No-Lines-Color-refactor" nil nil nil nil nil) nil nil t)
 
-(run-single '("No-Lines-Color-Box" nil nil nil nil nil) nil nil t)
+(run-single '("No-Lines-Color-Box-refactor" nil nil nil nil nil) nil nil t)
+
+; realtime use-model visible dolog
+(vote nil nil t nil)
 
 
-
-(run-single '("No-Lines-Color-Box" "all-perfect" "Top-To-Bottom-Left-To-Right-Box" "Relative-Positions-Color" "VG-Serial-Retrieve-Party" "click-closest-modified") t t t)
+(run-single '("No-Lines-Color-refactor" "all-perfect" "Top-To-Bottom-Left-To-Right-Box" "Relative-Positions-Color" "VG-Serial-Retrieve-Party" "click-closest-modified") t t t)
