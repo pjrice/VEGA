deecb681fd15c260d2019ca3ce4d788d35a05c9d
pjrice <pjr@rice.edu>
2022-03-01 16:47:58 -0600

    minor changes during testing

--- quickTest.lisp
+++ quickTest.lisp
@@ -8,12 +8,12 @@
 
 
 
-;(run-single '("No-Lines-Color-refactor" nil nil nil nil nil) nil nil t)
+(run-single '("No-Lines-Color-refactor" nil nil nil nil nil) nil nil t)
 
-(run-single '("No-Lines-Color-Box-refactor" nil nil nil nil nil) nil nil t)
+;(run-single '("No-Lines-Color-Box-refactor" nil nil nil nil nil) nil nil t)
 
 ; realtime use-model visible dolog
 (vote nil nil t nil)
 
 
-(run-single '("No-Lines-Color-refactor" "all-perfect" "Top-To-Bottom-Left-To-Right-Box" "Relative-Positions-Color" "VG-Serial-Retrieve-Party" "click-closest-modified") t t t)
+(run-single '("No-Lines-Color-Box-refactor" "all-perfect" "Top-To-Bottom-Left-To-Right-Box" "Relative-Positions-Color" "VG-Serial-Retrieve-Party" "click-closest-modified") t t t)
