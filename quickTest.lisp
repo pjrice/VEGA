16756f3bc20c6ed48398b8fd8a6d9d15f7023e11
pjrice <pjr@rice.edu>
2022-03-30 13:21:59 -0500

    update quickTest to test remaining ballot refactors

--- quickTest.lisp
+++ quickTest.lisp
@@ -1,6 +1,17 @@
-(setf *default-pathname-defaults* #P"/home/ausmanpa/")
+(cond ((string= (software-type) "Darwin") 
+        (progn
+            (setf *default-pathname-defaults* #P"/Users/pjr5/")
+            (load (merge-pathnames "Desktop/actr7.x/load-act-r.lisp"))
+        )
+      )
+	  ((string= (software-type) "Linux") 
+        (progn
+            (setf *default-pathname-defaults* #P"/home/ausmanpa/")
+            (load (merge-pathnames "actr7.x/load-act-r.lisp"))
+        )
+      )
+)
 
-(load (merge-pathnames "actr7.x/load-act-r.lisp"))
 
 (load (merge-pathnames "gp/VEGA/combine.lisp"))
 
@@ -12,9 +23,20 @@
 ;(run-single '("No-Lines-Color-Box-refactor" nil nil nil nil nil) nil nil t)
 ;(run-single '("WisconsinBallot-Box-refactor" nil nil nil nil nil) nil nil t)
 ;(run-single '("BrowardBallot-refactor" nil nil nil nil nil) nil nil t)
-
-(run-single '("WisconsinBallot-Instruction-refactor" nil nil nil nil nil) nil nil t)
-
+;(run-single '("WisconsinBallot-Instruction-refactor" nil nil nil nil nil) nil nil t)
+;(run-single '("WisconsinBallot-Instruction-Long-refactor" nil nil nil nil nil) nil nil t)
+;(run-single '("ballot_instructions_1-refactor" nil nil nil nil nil) nil nil t)
+;(run-single '("ballot_instructions_2-refactor" nil nil nil nil nil) nil nil t)
+;(run-single '("ballot_instructions_2_nofooter-refactor" nil nil nil nil nil) nil nil t)
+;(run-single '("ballot_instructions_3-refactor" nil nil nil nil nil) nil nil t)
+;(run-single '("ballot_instructions_3_nofooter-refactor" nil nil nil nil nil) nil nil t)
+;(run-single '("ballot_instructions_4-refactor" nil nil nil nil nil) nil nil t)
+(run-single '("ballot_instructions_2_plus-refactor" nil nil nil nil nil) nil nil t)
+;(run-single '("ballot_instructions_4_a1-refactor" nil nil nil nil nil) nil nil t)
+;(run-single '("ballot_instructions_4_a2-refactor" nil nil nil nil nil) nil nil t)
+;(run-single '("ballot_instructions_4_a4-refactor" nil nil nil nil nil) nil nil t)
+;(run-single '("ballot_instructions_4_plus-refactor" nil nil nil nil nil) nil nil t)
+;(run-single '("ballot_instructions_4_plus_nofooter-refactor" nil nil nil nil nil) nil nil t)
 
 ; realtime use-model visible dolog
 (vote nil nil t nil)
