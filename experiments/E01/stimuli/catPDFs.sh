54d4b1daaead73d7f5293e51859803495fef1cc1
pjrice <pjr@rice.edu>
2022-03-30 16:25:30 -0500

    creation of packets 16-27 for E01; there was a bug in catPDFs.sh where existing packets 10-15 were overwritten. These packets were restored from backup, and catPDFs.sh was updated to resolve bug. catPDFs.sh now requires two arguments; the first is the number of the first packet to make, and the second is the number of the last packet to make

--- experiments/E01/stimuli/catPDFs.sh
+++ experiments/E01/stimuli/catPDFs.sh
@@ -2,22 +2,28 @@
 # Randomize order of stimuli pdfs and cat them together with pdftk
 
 # script requires a single argument: the number of packets to make
-numPackets=$1
+#numPackets=$1
 
+# script now requires two arguments: the number of the first packet to make and the number of the last packet to make
+startVal=$1
+endVal=$2
+
+
+# this breaks because the existingPackets array contains the filenames numerically rather than by date; solution was to add startVal and endVal as arguments to script
 # get the filenames of existing packets to infer where numbering should start/end
-if [ -z "$(ls -A packets)" ]; then 
-    #echo "Empty" 
-    startVal=1
-    endVal=$numPackets
-else 
-    #echo "Not Empty" 
-    existingPackets=(packets/*)
-
-    lastPacketNum=${existingPackets[-1]:18:-4}
-
-    startVal=$((lastPacketNum + 1))
-    endVal=$((lastPacketNum + numPackets))
-fi
+#if [ -z "$(ls -A packets)" ]; then 
+#    #echo "Empty" 
+#    startVal=1
+#    endVal=$numPackets
+#else 
+#    #echo "Not Empty" 
+#    existingPackets=(packets/*)
+#
+#    lastPacketNum=${existingPackets[-1]:18:-4}
+#
+#    startVal=$((lastPacketNum + 1))
+#    endVal=$((lastPacketNum + numPackets))
+#fi
 
 
 cd pdf
